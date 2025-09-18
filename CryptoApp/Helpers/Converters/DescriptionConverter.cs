5e92fcfff87d223f3677a5141caa8767613d1934
Hosakoo <dimonsydoruk@gmail.com>
2025-09-18 08:51:29 +0300

    Add Description, List of markets, opportunity to switch to sites on the detail page

---
+++ CryptoApp/Helpers/Converters/DescriptionConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace CryptoApp.Helpers.Converters
+{
+    public class DescriptionConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value is string description && !string.IsNullOrWhiteSpace(description))
+                return description;
+            return "No description available";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- CryptoApp/ViewModels/CryptoDetailViewModel.cs
+++ CryptoApp/ViewModels/CryptoDetailViewModel.cs
@@ -4,6 +4,7 @@ using CryptoApp.Models;
 using CryptoApp.Services.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,12 +18,27 @@ namespace CryptoApp.ViewModels
 
         [ObservableProperty] private CryptoCurrencyDetail cryptoCurrency;
         [ObservableProperty] private bool isBusy;
+        
         public IRelayCommand BackCommand { get; }
+        public IRelayCommand OpenInBrowserCommand { get; }
+        public IRelayCommand OpenTickerInBrowserCommand { get; }
         public CryptoDetailViewModel(ICryptoApiService cryptoApiService, INavigationService navigationService)
         {
             _cryptoApiService = cryptoApiService;
             _navigationService = navigationService;
             BackCommand = new RelayCommand(() => _navigationService.GoBack());
+            OpenInBrowserCommand = new RelayCommand(() => OpenInBrowser());
+            OpenTickerInBrowserCommand = new RelayCommand<string>(url =>
+            {
+                if (!string.IsNullOrEmpty(url))
+                {
+                    Process.Start(new ProcessStartInfo
+                    {
+                        FileName = url,
+                        UseShellExecute = true
+                    });
+                }
+            });
         }
 
         public void ReceiveParameter(object parameter)
@@ -30,6 +46,17 @@ namespace CryptoApp.ViewModels
             if (parameter is CryptoCurrencyDetail detail)
             {
                 CryptoCurrency = detail;
+            }
+        }
+
+        public void OpenInBrowser() { 
+            if(!string.IsNullOrWhiteSpace(CryptoCurrency.Links.Homepage.First()))
+            {
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = CryptoCurrency.Links.Homepage.First(),
+                    UseShellExecute = true
+                });
             }
         }
     }
